a09702208a97e76e1c173f9513c2a23ea55791fe
Meet Godhani <meetgodhani23@gmail.com>
2017-08-19 12:02:41 -0400

    Initial Thiiiiiings added

---
+++ src/renderer/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rss-reader',
+};
+</script>
+
+<style lang="stylus">
+@import './assets/_font'
+@import '../../node_modules/vuetify/src/stylus/settings/_colors'
+$theme := {
+  primary: $grey.darken-4
+  accent: $red.accent-2
+  secondary: $pink.darken-1
+  info: $blue.lighten-1
+  warning: $amber.darken-2
+  error: $red.accent-4
+  success: $green.lighten-2
+}
+@import '../../node_modules/vuetify/src/stylus/main'
+</style>
